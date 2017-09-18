c1efc560f71c6c9273a64adbd317c6291574e952
dhungelsumit <dhungelsumit2014@gmail.com>
2017-09-18 02:06:17 -0400

    Initial commit

---
+++ Codelab3/lab3_part1.cpp
@@ -0,0 +1,45 @@
+// Program to generate look and say sequence
+// https://en.wikipedia.org/wiki/Look-and-say_sequence
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+string LookAndSay(int n){
+    if (n == 0) {
+      return "";
+    }
+    if (n == 1) {
+      return "1";
+    }
+    string previous = "1";
+    int counter = 1;
+    while (counter < n) {
+        string current = "";
+        for (int i = 0; i < previous.length(); i++) {
+            int count = 1;
+             while ((i + 1 < previous.length()) && (previous[i] == previous[i + 1])){
+                i++;
+                count++;    
+            }
+            current += to_string(count) + previous[i];
+        }
+        previous = current;
+        counter++;
+    }
+    return previous;
+}
+
+
+int main(){
+  int number;
+  string result;
+  cout << "Enter the value of n";
+  cin >> number;
+  cout << endl;
+  result = LookAndSay(number);
+  cout << "The "<< number<<" number in the series is "<<result<<endl;
+  return 0;
+  }
