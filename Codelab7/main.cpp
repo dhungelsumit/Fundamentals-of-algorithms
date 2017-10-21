d7270c9a15e69eebed22eeb8c75a101912a91b0b
dhungelsumit <dhungelsumit2014@gmail.com>
2017-10-21 16:56:15 -0400

    Implement inorder, preorder and postorder functionality

--- Codelab7/main.cpp
+++ Codelab7/main.cpp
@@ -16,18 +16,27 @@ struct Node* newNode(int data){
 };
 
 void inorder(Node* root){
-    
+    if (root) {
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+     }
 }
 
 void preorder(Node* root){
-
-
+    if (root) {
+        cout << root->data << " ";
+        preorder(root->left);
+        preorder(root->right);
+     }
 }
 
 void postorder(Node* root){
-
-
-
+    if (root) {
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->data << " ";        
+     }
 }
 
 void BFS_tree(Node* root){
@@ -54,10 +63,12 @@ int main(){
     root->left->right = newNode(5);
     root->right->left = newNode(6);
     root->right->right = newNode(7);
-// The calls for the above incomplete functions commented out purposefully. Please uncomment after you have completed all the above methods.
-//    inorder(root);
-//    preorder(root);
-//    postorder(root);
+    inorder(root);
+    cout << endl;    
+    preorder(root);
+    cout << endl;    
+    postorder(root);
+    cout << endl;    
 //    BFS_tree(root);
 //    all_root_to_leaves(root);
 //    sumPath(root, 11);
