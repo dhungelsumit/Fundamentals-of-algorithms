c451de1ea47cf77f1c4f22bd4b4d269cf4f00fcf
dhungelsumit <dhungelsumit2014@gmail.com>
2017-11-06 00:38:55 -0500

    Dynamic Programming Solution

--- Codelab8/word_wrap_dp.cpp
+++ Codelab8/word_wrap_dp.cpp
@@ -1,7 +1,12 @@
-//Process the provided input text below and lay it out in lines of a specified maximum length.
+/** Implement a dynamic programming solution that provides the optimal "pretty printing" text layout that optimizes the provided cost function. 
+ * References:
+ * http://www.geeksforgeeks.org/dynamic-programming-set-18-word-wrap/
+ * https://www.youtube.com/watch?v=RORuwHiblPc&t=822s
+ */
 
-#include <iostream>
 #include <fstream>
+#include <math.h>
+#include <iostream>
 #include <string>
 #include <sstream>
 #include <vector>
@@ -25,20 +30,20 @@ vector<string> split(string sentence, string delimiter){
     return list;
 }
 
-void greedy(int max_length){
+// Separate out all the words from the text file
+vector<string> AllWords(int max_length){
     ifstream inFile;//input text
     inFile.open("input_text.txt");//opening the input text
     if (inFile.fail()) {
 		cout << "Problem opening file: " << endl;
 		exit(1);
 	}
-    ofstream outFile;//output text
-    outFile.open ("output_text.txt");//opening the output text
     string temp_word = "";
     string temp_sentence ="";
     int word_len = 0;
     int line_len = 0;
     int counter = 0;
+    vector <string> all_words;
     while(!inFile.eof()){
         getline(inFile, temp_sentence);
         counter++;
@@ -47,39 +52,74 @@ void greedy(int max_length){
         for(int i = 0; i < word_list.size(); i++){
             temp_word = word_list[i];
             word_len = temp_word.length();
-            line_len += word_len;
              // If the length of word is greater than the maximum length of line.
              if (word_len > max_length){
                 cout << "Error: The file contain very long word. Please remove it "<<endl;
                 exit(1);
             }
-            if (line_len > max_length){
-                outFile << "\n";
-                line_len = word_len;
-                outFile << temp_word;
-                line_len++;
-                outFile <<" ";                
-             }
-             else{
-                outFile << temp_word;
-                line_len += 1;
-                outFile <<" ";
-             }
+            all_words.push_back(temp_word);
     }
-    line_len = 0;
-    word_len = 0;
-        // Change of paragraph.
-        if (counter > 1){
-            outFile << "\n";
+    }
+    return all_words;
+}
+
+void WordWrap(int max_length){
+    vector <string> words = AllWords(max_length);
+    // (M - l​1​)​^2​ + (M - l​2​)​^2​ + ... + (M - l​(k-1​))​^2​ . 
+    int cost[words.size()][words.size()];
+    // Calculating cost
+    for(int i=0 ; i < words.size(); i++){
+        // Max Length minus length of the word ( M - l(n))
+        cost[i][i] = max_length - words[i].length();
+        for(int j=i+1; j < words.size(); j++){
+            cost[i][j] = cost[i][j-1] - words[j].length() - 1; 
         }
     }
-
+    for(int i=0; i < words.size(); i++){
+        for(int j=i; j < words.size(); j++){
+            if(cost[i][j] < 0){
+                // If word doesn't fit one line, add maximum integer value
+                cost[i][j] = INT_MAX;
+            }else{
+                cost[i][j] = pow(cost[i][j], 2);
+            }
+        }
+    }
+    int minimum_cost[words.size()];
+    int result[words.size()];
+    // Finding minimum cost
+    for(int i = words.size()-1; i >= 0 ; i--){
+        minimum_cost[i] = cost[i][words.size()-1];
+        result[i] = words.size();
+        for(int j=words.size()-1; j > i; j--){
+            if(cost[i][j-1] == INT_MAX){
+                continue;
+            }
+            if(minimum_cost[i] > minimum_cost[j] + cost[i][j-1]){
+                minimum_cost[i] = minimum_cost[j] + cost[i][j-1];
+                result[i] = j;
+            }
+        }
+    }
+    int i = 0;
+    int j;
+    ofstream outFile;//output text
+    outFile.open ("output_text.txt");//opening the output text
+    do{
+        j = result[i];
+        for(int k=i; k < j; k++){
+            outFile << words[k];
+            outFile <<  " ";
+        }
+        outFile << "\n";
+        i = j;
+    }while(j < words.size());
 }
 
 int main(){
     int max_length;
     cout << "Enter the maximum length of each line (maximum character possible)" << endl;
     cin >> max_length;
-    greedy(max_length);
+    WordWrap(max_length);
     return 0;
 }
